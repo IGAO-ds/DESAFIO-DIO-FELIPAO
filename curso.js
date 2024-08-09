32bde54318d2dceac0dc18fbb3c2e3d0b1128e09
IGAO-ds <igoridsousa@gmail.com>
2024-08-09 17:41:05 -0300

    meu segundo

--- curso.js
+++ curso.js
@@ -1,4 +1,45 @@
-console.log("ola")
+let HEROI = {
+    nome: "DIO-HERO",
+    xp: 2000,
+    rank: "indeterminado"
+}
 
 
+switch(true){
+    case (HEROI.xp<= 1000):
+     HEROI.rank = "ferro"
+     console.log("Parabens, seu novo rank 'e: " + HEROI.rank + ", Pratique mais")
+    break
+    
+    case (HEROI.xp>= 1001 && HEROI.xp <= 2000):
+     HEROI.rank = "bronze"
+     console.log("Parabens, seu novo rank 'e: " + HEROI.rank + ", a carroagem ta comecando a andar")
+    break
 
+    case (HEROI.xp>= 2001 && HEROI.xp <= 5000):
+     HEROI.rank = "Prata Ouro"
+     console.log("Parabens, seu novo rank 'e: " + HEROI.rank + ", nao se iluda, ainda tem chao")
+    break
+
+    case (HEROI.xp>= 5001 && HEROI.xp <= 8000):
+     HEROI.rank = "Platina Diamante"
+     console.log("Parabens, seu novo rank 'e: " + HEROI.rank + ", pode comecar a se gabar")
+    break
+
+    case (HEROI.xp>= 8001 && HEROI.xp <= 9000):
+     HEROI.rank = "Ascendente"
+     console.log("Parabens, seu novo rank 'e: " + HEROI.rank + ", melhor que muitos")
+    break
+
+    case (HEROI.xp>= 9001 && HEROI.xp <= 10000):
+     HEROI.rank = "Imortal"
+     console.log("Parabens, seu novo rank 'e: " + HEROI.rank + ", destruidor de lares")
+    break
+
+    case (HEROI.xp>= 10001):
+     HEROI.rank = "Radiante"
+     console.log("Parabens, seu novo rank 'e: " + HEROI.rank + ", TU E MAIS QUANTOS?")
+    break
+
+}
+ 
